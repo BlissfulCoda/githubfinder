164cba508bf0fa5b7b06ab6d100ffe8952e79686
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2023-02-03 17:22:09 +0000

    feat(client): display single user profile page mobile

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <GithubProvider>
       <Router>
-        <div className="h-screen flex flex-col justify-between bg-black bg-dark-image bg-no-repeat bg-center bg-[length:650px_650px] contrast-200 saturate-200  bg-blend-hard-light overflow-hidden">
+        <div className="h-screen flex flex-col justify-between bg-black bg-dark-image bg-no-repeat bg-center bg-[length:600px_600px] contrast-200 saturate-200  bg-blend-hard-light overflow-hidden">
           <main>
             <Routes>
               <Route path="/" element={<Home />} />
--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,8 +1,17 @@
 import { useContext, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { FaLongArrowAltLeft, FaUser } from "react-icons/fa";
+import {
+  BsFillStarFill,
+  BsEye,
+  BsArrowLeft,
+  BsBoxArrowUpRight,
+} from "react-icons/bs";
 import Footer from "../Components/Layout/Footer";
 import Button from "../Components/Shared/Button";
+import { UserInterface } from "../Context/Github/GithubContextData";
+import Spinner from "../Components/Shared/Spinner";
+import JohnWick from "../assets/JohnWick.png";
 
 import GithubContext from "../Context/Github/GithubContextData";
 import { GithubContextDataInterface } from "../Context/Github/GithubContextData";
@@ -13,32 +22,168 @@ export default function Profile(): JSX.Element {
   ) as GithubContextDataInterface;
 
   const params = useParams();
-  const login: string = params.login!
+  const userLogin: string = params.login!;
 
-  
+  const {
+    name,
+    avatar_url,
+    followers,
+    following,
+    public_repos,
+    login,
+    bio,
+    location,
+    hireable,
+    website
+  }: UserInterface = user;
 
   useEffect(() => {
-    fetchUser(login);
+    fetchUser(userLogin);
+    console.log(user);
   }, []);
 
-  return (
-    <div className="p-8">
-      <nav className="flex justify-between">
-        <Link to={`/user`}>
-          <Button>
-            <FaLongArrowAltLeft className="text-xl text-yellow-500 bg-slate-600 rounded-full p-1" />
-          </Button>
-        </Link>
-        <div className="text-xs">
-          <div className="flex space-x-2 items-center">
-            <FaUser className="text-sky-800 " />
-            <h1 className="text-green-300">{login}</h1>
-          </div>
-        </div>
-      </nav>
-      {/* PROFILE DISPLAY */}
+  if (loading) return <Spinner />;
 
-      <Footer />
-    </div>
+  return (
+    <section className="mx-9 mt-4 relative h-screen">
+      <figure>
+        <img
+          src={avatar_url}
+          alt={`image of ${userLogin}`}
+          className={`object-center h-[450px] w-full object-cover object-top  brightness-50 contrast-50`}
+        />
+      </figure>
+      <div className="w-full absolute top-2 right-0 h-full p-2 pt-5">
+        <nav className="flex justify-between">
+          <Link to="/">
+            <Button>
+              <BsArrowLeft className="text-2xl text-yellow-500 bg-[#1D15E] rounded-full p-1" />
+            </Button>
+          </Link>
+
+          <div className="flex space-x-3 items-center text-[9px] p-1">
+            <FaUser className="text-sky-900" />
+            <h1
+              className={`font-light ${
+                hireable ? "text-green-400" : "text-red-400"
+              }`}
+            >
+              Hireable
+            </h1>
+          </div>
+        </nav>
+
+        <div className="flex text-white text-[7px] space-x-2 border-[0.1px] border-opacity-10 border-y-neutral-50 border-x-0 py-2 font-light w-48 text-right ml-auto pl-5">
+          <h4>
+            {followers} <span className="opacity-40">Followers</span>
+          </h4>
+          <h4>
+            {following} <span className="opacity-40">Following</span>
+          </h4>
+          <h4>
+            {public_repos} <span className="opacity-40">Public Repos</span>
+          </h4>
+        </div>
+
+        {/* PROFILE DISPLAY */}
+        <div className="w-full absolute bottom-12 right-0 opacity-90">
+          <div className="space-y-6 mb-1">
+            <div className="flex text-white text-[11px] justify-between opacity-50 text-thin pr-1">
+              <h3 className="">Latest Repositories</h3>
+              <div className="flex space-x-6">
+                <h3>{location}</h3>
+                <h3>Website</h3>
+                <h3>Github</h3>
+              </div>
+            </div>
+
+            {/* REPOSITORIES */}
+            <div className="space-y-1">
+              <div className="bg-[#0C0E15] bg-opacity-75 border-l-4 border-l-violet-700 w-full h-28 p-3 rounded-lg p-2">
+                {/* LEFT SIDE */}
+                <div className="flex justify-between ml-1">
+                  <div className="flex space-x-2">
+                    {/* STARS */}
+                    <span className="flex space-x-1">
+                      <BsFillStarFill className="text-amber-300 text-[9px]" />{" "}
+                      <h6 className="text-white text-[8px]">24</h6>
+                    </span>
+                    {/* EYES */}
+                    <span className="flex space-x-1">
+                      <BsEye className="text-blue-600 text-[13px]" />{" "}
+                      <h6 className="text-white text-[8px]">302</h6>
+                    </span>
+                  </div>
+
+                  {/* RIGHT SIDE */}
+                  <div className="flex space-x-2">
+                    <BsBoxArrowUpRight className="text-violet-600 text-xs" />
+                    <h6 className="text-white text-[10px]">Project Name</h6>
+                  </div>
+                </div>
+                <h5 className="text-white text-xs text-center mt-4">
+                  Welcome to this Project
+                </h5>
+              </div>
+
+              <div className="bg-[#0C0E15] bg-opacity-75 border-l-4 border-l-violet-700 w-full h-28 p-3 rounded-lg p-2">
+                {/* LEFT SIDE */}
+                <div className="flex justify-between ml-1">
+                  <div className="flex space-x-2">
+                    {/* STARS */}
+                    <span className="flex space-x-1">
+                      <BsFillStarFill className="text-amber-300 text-[9px]" />{" "}
+                      <h6 className="text-white text-[8px]">24</h6>
+                    </span>
+                    {/* EYES */}
+                    <span className="flex space-x-1">
+                      <BsEye className="text-blue-600 text-[13px]" />{" "}
+                      <h6 className="text-white text-[8px]">302</h6>
+                    </span>
+                  </div>
+
+                  {/* RIGHT SIDE */}
+                  <div className="flex space-x-2">
+                    <BsBoxArrowUpRight className="text-violet-600 text-xs" />
+                    <h6 className="text-white text-[10px]">Project Name</h6>
+                  </div>
+                </div>
+                <h5 className="text-white text-xs text-center mt-4">
+                  Welcome to this Project
+                </h5>
+              </div>
+
+              <div className="bg-[#0C0E15] bg-opacity-75 border-l-4 border-l-violet-700 w-full h-28 p-3 rounded-lg p-2">
+                {/* LEFT SIDE */}
+                <div className="flex justify-between ml-1">
+                  <div className="flex space-x-2">
+                    {/* STARS */}
+                    <span className="flex space-x-1">
+                      <BsFillStarFill className="text-amber-300 text-[9px]" />{" "}
+                      <h6 className="text-white text-[8px]">24</h6>
+                    </span>
+                    {/* EYES */}
+                    <span className="flex space-x-1">
+                      <BsEye className="text-blue-600 text-[13px]" />{" "}
+                      <h6 className="text-white text-[8px]">302</h6>
+                    </span>
+                  </div>
+
+                  {/* RIGHT SIDE */}
+                  <div className="flex space-x-2">
+                    <BsBoxArrowUpRight className="text-violet-600 text-xs" />
+                    <h6 className="text-white text-[10px]">Project Name</h6>
+                  </div>
+                </div>
+                <h5 className="text-white text-xs text-center mt-4">
+                  Welcome to this Project
+                </h5>
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </section>
   );
 }
