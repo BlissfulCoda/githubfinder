58135386a27d1aaa03d590622678a818bfe3dea4
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2023-02-07 11:10:59 +0000

    fix(client): clean up code

--- src/Components/Users/UserResults.tsx
+++ src/Components/Users/UserResults.tsx
@@ -12,7 +12,7 @@ export default function UserResults(): JSX.Element {
 
   if (!loading) {
     return (
-      <div className="relative h-[800px] w-[400px] sm:w-full mx-auto overflow-y-auto overflow-x-hidden">
+      <div className="relative h-[800px] w-[450px] sm:w-full mx-auto overflow-y-auto overflow-x-hidden">
         <div className="absolute top-0 -left-12 container grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 gap-0 px-1 lg:grid-cols-6">
           {users.map((user) => (
             <User key={user.id} user={user} />
