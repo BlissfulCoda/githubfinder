22f3fc1b3adc1fd0351a547b2e6776310a54e51c
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2023-02-07 10:25:26 +0000

    feat(client): fix some bugs

--- src/Components/Users/UserSearch.tsx
+++ src/Components/Users/UserSearch.tsx
@@ -49,7 +49,7 @@ export default function UserSearch(): JSX.Element {
           <>
             <input
               type="text"
-              className="bg-black border border-x-0 border-t-0 focus:outline-none border-b-zinc-500 text-neutral-content placeholder:text-[9px] text-xs focus:placeholder:text-[7px] text-white"
+              className="bg-black border border-x-0 border-t-0 focus:outline-none border-b-zinc-500 text-neutral-content placeholder:text-[9px] text-sm focus:placeholder:text-[7px] text-white"
               placeholder="Search a user..."
               onChange={handleChange}
               value={text}
