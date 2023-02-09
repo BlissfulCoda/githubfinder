6e48095ef8cbb298cca1dda45dc8e13136855f02
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2023-02-09 14:16:29 +0000

    fix(client): removed images

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -12,7 +12,7 @@ export default function Home(): JSX.Element {
   return (
     <section className="md:h-full w-full mx-auto sm:flex md:flex-col md:justify-center sm:items-center p-3">
       <section
-        className="container flex flex-col  justify-between sm:mx-2 md:mx-0 px-1 md:h-4/5 lg:h-5/6 md:max-w-3xl lg:max-w-4xl xl:max-w-4xl profile-border md:bg-black  lg:w-[900px] xl:w-[1000px] md:h-4/5 lg:h-5/6 xl:h-5/6 sm:bg-Earth-New bg-no-repeat bg-[length:400px_400px] bg-center bg-[center_top_9rem] sm:bg-[length:500px_500px] sm:bg-[center_top_8rem] contrast-100 saturate-150 
+        className="container flex flex-col  justify-between sm:mx-2 md:mx-0 px-1 md:h-4/5 lg:h-5/6 md:max-w-3xl lg:max-w-4xl xl:max-w-4xl profile-border md:bg-black  lg:w-[900px] xl:w-[1000px] md:h-4/5 lg:h-5/6 xl:h-5/6 sm:bg-Earth-New bg-no-repeat md:bg-opacity-50 bg-[length:400px_400px] bg-center bg-[center_top_9rem] sm:bg-[length:500px_500px] sm:bg-[center_top_8rem] contrast-100 saturate-100 
      md:bg-[length:550px_550px] md:bg-[center_top_2rem] lg:bg-[length:590px_590px] lg:bg-[center_top_1rem] xl:bg-[length:660px_660px] xl:bg-[center_top_-1rem] overflow-hidden"
       >
         <Navbar />
